902a333fddfa4a9eaba8a222de96703cd68ffcad
strasse34 <rmirzaie2021@gmail.com>
2023-11-02 15:10:27 +0000

    search challenge done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import css from './App.module.css';
-import SearchBar from './components/SearchBar';
+import Content from './components/Content';
+
 
 function App() {
   return (
     <div className={css.App}>
       
-      <SearchBar />
+      <Content />
     </div>
   );
 }
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,16 +8,28 @@ export class Content extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        isLoaded: false
+        isLoaded: false,
+        posts: []
       }
     }
 
-    LoadConvertor() {
-      setTimeout(() => {this.setState({isLoaded: true})}, 2000)
-    }
+    handleChange = (event) => {
+      const name = event.target.value.toLowerCase();
+      const filteredPosts = savedPosts.filter(post => {
+        return post.name.toLowerCase().includes(name);
+      })
+      this.setState({
+        posts: filteredPosts
 
+      })
+    }
   componentDidMount() {
-      this.LoadConvertor()
+      setTimeout(() => {this.setState({
+        isLoaded: true,
+        posts: savedPosts
+      })}, 2000)
+      
+
   } 
 
   render() {
@@ -26,9 +38,18 @@ export class Content extends Component {
       <div className={css.Content}>
             <div className={css.TitleBar}>
                 <h1>My Photos</h1>
+                <form>
+                  <label>Search: <input 
+                  type='search'
+                  id='searchInput' 
+                  placeholder='By Author' 
+                  onChange={(event) => this.handleChange(event)}
+                  /></label>
+                </form>
+                <h4>posts found: {this.state.posts.length}</h4>
             </div>
             <div className={css.SearchResults}>
-            {this.state.isLoaded ? (<PostItem savedPosts={savedPosts} />) : (<Loader />)}
+            {this.state.isLoaded ? (<PostItem savedPosts={this.state.posts} />) : (<Loader />)}
             </div>
       </div>
     )
